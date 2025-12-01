4d26739ed5b5ce50807d05378247be728916aba4
ismael <ismaelbernadinojk07@gmail.com>
2025-12-01 12:31:03 -0300

    Add files via upload

---
+++ ItemResponse.cs
@@ -0,0 +1,18 @@
+﻿using PerdiNoCampus.API.Models;
+
+namespace PerdiNoCampus.API.Contracts
+{
+    public class ItemResponse
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public ECategoriaItem CategoriaItem { get; set; }
+        public string LocalEncontrado { get; set; }
+        public ETurno TurnoEncontrado { get; set; }
+        public string UsarioNomeLocalizou { get; set; }
+        public string ImagemUrl { get; set; }
+        public bool? FoiRecuperado { get; set; }
+        public bool? FoiEntregueAPrefeitura { get; set; }
+        public DateTime CriadoEm { get; set; }
+    }
+}
